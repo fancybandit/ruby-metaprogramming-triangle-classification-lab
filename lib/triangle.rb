f6225d5778764a0a92abae3fe32ab5387837dd31
Jacob Klimenko <droidedjake@gmail.com>
2020-11-22 17:10:07 -0500

    commit tree 0a9e767e969e2fda3d527714476a13c4003f2c8e

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,45 @@
+
 class Triangle
-  # write code here
+
+  attr_reader :lengths
+
+  def initialize(length_1, length_2, length_3)
+    @lengths = Array.new.insert(0, length_1, length_2, length_3)
+  end
+
+  def kind
+    if exists? && tri_inequality?
+      if all_equal?
+        :equilateral
+      elsif no_equal?
+        :scalene
+      else
+        :isosceles
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
+  def exists?
+    self.lengths.all? {|l| l > 0}
+  end
+
+  def tri_inequality?
+    self.lengths.each_with_index.all? do |len, i|
+      len + self.lengths[i-1] > self.lengths[i-2]
+    end
+  end
+
+  def all_equal?
+    self.lengths.uniq.size <= 1
+  end
+
+  def no_equal?
+    self.lengths.uniq.size == 3
+  end
 end
+
